9b0cd34ac0f3e21bb5a1db1ebc41d9561ba2fd8c
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-04-18 12:34:34 +0700

    Add route params

--- src/helpers/RouteElement.js
+++ src/helpers/RouteElement.js
@@ -1,10 +1,11 @@
 import React from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import {useLocation, useNavigate, useParams} from "react-router-dom";
 
 const RouteElement = ({component: RouteComponent, ...props}) => {
     const location = useLocation();
     const navigate = useNavigate();
-    return <RouteComponent {...props} location={location} navigate={navigate} />;
+    const params = useParams();
+    return <RouteComponent {...props} location={location} navigate={navigate} params={params} />;
 };
 
 export default RouteElement;
